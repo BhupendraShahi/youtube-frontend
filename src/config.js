2f18c4c6aa5f3396e813b607f5062bf35a3e7205
Bhupendra <shahi23.bhupendra11@gmail.com>
2023-08-24 11:28:15 +0530

    updated backedn base URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://youtube-clone-wsij.onrender.com"
+    baseURL: "https://youtube-clone-wsij.onrender.com/api"
 });
