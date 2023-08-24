57511880ab0f16e7988ae048e8cc16822ca7f574
Bhupendra <shahi23.bhupendra11@gmail.com>
2023-08-24 11:23:38 +0530

    updated backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://not-youtube-server.herokuapp.com/api/"
+    baseURL: "https://youtube-clone-wsij.onrender.com"
 });
